f1d769fa66e4fa595dad6a405fc7ba651124c0f2
mero <piotr.luczko@gmail.com>
2015-09-17 23:42:04 +0200

    requirejs

--- assets/js/require.js
+++ assets/js/require.js
@@ -4,7 +4,15 @@ require.config({
     jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
     bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
     fancybox: '//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack'
-  }
+  },
+  shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'fancybox' : {
+        	deps: ['jquery']
+        }
+    }
 });
 
 // google analytics
