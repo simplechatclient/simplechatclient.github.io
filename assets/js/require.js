f32f22b59a4a95ad1f42dd4a93ca41a18cc4c0ac
mero <piotr.luczko@gmail.com>
2015-09-18 00:03:59 +0200

    code clean

--- assets/js/require.js
+++ assets/js/require.js
@@ -1,16 +1,16 @@
 require.config({
-  paths: {
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
-    bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
-    fancybox: '//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack'
-  },
-  shim: {
-	    'bootstrap': {
-	        deps: ['jquery']
-	    },
-	    'fancybox' : {
-	    	deps: ['jquery']
-	    }
+	paths: {
+		jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
+		bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
+		fancybox: '//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack'
+	},
+	shim: {
+		'bootstrap': {
+			deps: ['jquery']
+		},
+		'fancybox' : {
+			deps: ['jquery']
+		}
 	}
 });
 
