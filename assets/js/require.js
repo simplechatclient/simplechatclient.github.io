b33c281adf7c41b6d26366557a2b645d852abdfa
mero <piotr.luczko@gmail.com>
2015-09-17 23:50:45 +0200

    requirejs

--- assets/js/require.js
+++ assets/js/require.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-  	baseUrl: '/assets/js'
+  	baseUrl: '/assets/js',
     jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
     bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
     fancybox: '//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack'
