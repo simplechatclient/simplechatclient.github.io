b8635cfd2bbfd597f4a14c02882fc1df8fff4284
mero <piotr.luczko@gmail.com>
2015-09-17 23:48:16 +0200

    requirejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,41 @@
 define(['jquery', 'bootstrap', 'fancybox'], function($, bootstrap, fancybox){
 
-	$(document).ready(function() {
-		fancybox();
-		platform();
-		buttons();
-	});
+	var initialize = function(){
+		$(document).ready(function() {
+			fancybox();
+			platform();
+			buttons();
+		});
 
-	function fancybox()
-	{
-		if(typeof $.fancybox == 'function') {
-			$('.fancybox').fancybox();
+		function fancybox()
+		{
+			$('.fancybox').fancybox();	
+		}
+
+		function platform()
+		{
+			var isMobile = jQuery.browser.mobile;
+			var isWindows = navigator.platform.toUpperCase().indexOf('WIN')!==-1;
+			
+			if (isMobile) { $('#mobile').show(); }
+			else if (isWindows) { $('#windows').show(); }
+			else { $('#linux').show(); }
+		}
+
+		function buttons()
+		{
+			$('#other_systems').click(function() {
+				$('#windows').show();
+				$('#linux').show();
+				$('#mobile').show();
+				
+				$('#more').show();
+				$('#other_systems_row').hide();
+			});
 		}
 	}
-
-	function platform()
-	{
-		var isMobile = jQuery.browser.mobile;
-		var isWindows = navigator.platform.toUpperCase().indexOf('WIN')!==-1;
-		
-		if (isMobile) { $('#mobile').show(); }
-		else if (isWindows) { $('#windows').show(); }
-		else { $('#linux').show(); }
-	}
-
-	function buttons()
-	{
-		$('#other_systems').click(function() {
-			$('#windows').show();
-			$('#linux').show();
-			$('#mobile').show();
-			
-			$('#more').show();
-			$('#other_systems_row').hide();
-		});
-	}
+	return {
+    	initialize: initialize
+  	};
 });
 
