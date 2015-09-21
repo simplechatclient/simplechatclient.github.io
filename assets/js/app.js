929c8259740ae2232283f61fdc44c1d5f24a2243
mero <piotr.luczko@gmail.com>
2015-09-22 01:03:34 +0200

    strict js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+"use strict";
 define(['jquery', 'bootstrap', 'fancybox'], function($, bootstrap, fancybox){
 
 	$(document).ready(function() {
@@ -57,4 +58,3 @@ define(['jquery', 'bootstrap', 'fancybox'], function($, bootstrap, fancybox){
 		(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
 	}
 });
-
--- assets/js/require.js
+++ assets/js/require.js
@@ -1,3 +1,4 @@
+"use strict";
 require.config({
 	paths: {
 		jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
