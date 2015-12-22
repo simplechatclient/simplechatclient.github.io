bcd7056d6f17c32e8ca309523431e660db50248c
mero <piotr.luczko@gmail.com>
2015-12-22 22:32:26 +0100

    libs updated

--- assets/js/require.js
+++ assets/js/require.js
@@ -2,7 +2,7 @@
 require.config({
 	paths: {
 		jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
-		bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min',
+		bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min',
 		fancybox: '//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack'
 	},
 	shim: {
