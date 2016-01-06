18529f3263eb7f2ab6d61693b18c201bb3af393d
mero <piotr.luczko@gmail.com>
2016-01-06 17:39:38 +0100

    scripts fix

--- _assets/js/scc.js
+++ _assets/js/scc.js
@@ -47,7 +47,6 @@ function buttons()
 
 function google_analytics()
 {
-	// google analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 	ga('create', 'UA-64667530-1', 'auto');
@@ -56,12 +55,10 @@ function google_analytics()
 
 function twitter()
 {
-	// twitter
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 }
 
 function facebook()
 {
-	// facebook
 	(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
 }
