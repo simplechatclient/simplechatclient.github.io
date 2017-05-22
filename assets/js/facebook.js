a1c4ac73c05afde75b5097f5b00cc85b99e86e01
piotrluczko <piotr.luczko@gmail.com>
2017-05-23 01:29:50 +0200

    updated icons

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,1 +1,1 @@
-!function(e,t,n){var o,c=e.getElementsByTagName(t)[0];e.getElementById(n)||(o=e.createElement(t),o.id=n,o.src="//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.7",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
+!function(e,t,n){var o,c=e.getElementsByTagName(t)[0];e.getElementById(n)||((o=e.createElement(t)).id=n,o.src="//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.7",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
